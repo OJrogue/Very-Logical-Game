2d619630b4f7782279074c815c30bcd46c104c19
_OJ_ <ojytcanal@gmail.com>
2022-03-15 21:27:28 +0100

    PWA

---
+++ public/service-worker.js
@@ -0,0 +1,6 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+
+workbox.routing.registerRoute(
+    ({request}) => request.destinacion === 'image',
+    new workbox.strategies.CacheFirst()
+);
